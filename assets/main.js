cd84bfeb1fbdf6459529bbc3fac000b83dcbe21f
juanfernandez2306 <juanfernandez2306@gmail.com>
2022-06-23 06:52:27 -0400

    add file img and remove plugin slider for swiper

--- assets/main.js
+++ assets/main.js
@@ -6,6 +6,22 @@ let load = () => {
         element.classList.toggle('open');
     }, false);
 
+    const swiper = new Swiper('.swiper', {
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: true
+        },
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+      
+
 };
 
 window.addEventListener('load', load, false);
